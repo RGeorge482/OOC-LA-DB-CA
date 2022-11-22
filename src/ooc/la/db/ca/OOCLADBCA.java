ed44ee17661031acdbeb274ddb77664f24f9da63
RGeorge482 <ratagheorghita@yahoo.com>
2022-11-22 20:30:19 +0000

    Initial commit

---
+++ src/ooc/la/db/ca/OOCLADBCA.java
@@ -0,0 +1,17 @@
+
+package ooc.la.db.ca;
+
+/**
+ *
+ * @author 35386
+ */
+public class OOCLADBCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
